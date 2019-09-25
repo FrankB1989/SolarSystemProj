01b558211e11389ac3dac01e1cf91258158b7c20
FrankB1989 <50275340+FrankB1989@users.noreply.github.com>
2019-09-25 17:12:04 +0200

    Added Solar System Project
    Added Solar System Simulation Project to Git Hub

---
+++ Assets/Scripts/RotatePluto.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePluto : MonoBehaviour {
+
+    public Transform target;
+    public int speed;
+
+    private Vector3 angle;
+
+    // Use this for initialization
+    void Start () {
+		if(target==null)
+        {
+            target = this.gameObject.transform;
+        }
+        angle.x = target.transform.up.x;
+        angle.y = 45;
+        angle.z = 45;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(target.transform.position, angle, speed * Time.deltaTime);
+	}
+}
